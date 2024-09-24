f351b0a09675066efa23c3493730df52f2448ef0
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-23 21:36:05 -0400

    Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AndyJavier_AP1_P1.Models;
+using Microsoft.Win32;
+
+namespace AndyJavier_AP1_P1.DAL;
+
+public class Contexto : DbContext
+{
+
+    public Contexto(DbContextOptions<Contexto> options)
+        : base(options) { }
+    public DbSet<Registro> Registros { get; set; }
+}
