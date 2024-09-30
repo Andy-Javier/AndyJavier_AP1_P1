7fd570f796eb13384130b246e99d3f023617c9f4
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-30 18:38:17 -0400

    cree el Modelo Prestamo

---
+++ Models/Prestamo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AndyJavier_AP1_P1.Models
+{
+    public class Prestamo
+    {
+        [Key]
+        public int PrestamoId { get; set; }
+        [Required]
+        public string? Deudor { get; set; }
+        [Required]
+        public string? Concepto { get; set; }
+        [Required]
+        public int Monto { get; set; }
+        
+    }
+}
