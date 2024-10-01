6d02e3c1f658479c9ec273d34adb4223d6c5cdd3
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-30 20:28:46 -0400

    Modifique el Program para que funcione Bootstrap

--- Program.cs
+++ Program.cs
@@ -2,39 +2,41 @@ using AndyJavier_AP1_P1.Components;
 using AndyJavier_AP1_P1.DAL;
 using Microsoft.EntityFrameworkCore;
 
-namespace AndyJavier_AP1_P1
+namespace AndyJavier_AP1_P1;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var conStr = builder.Configuration.GetConnectionString("ConStr");
+        builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(conStr));
+
+        // Registrar PrestamoServices
+        builder.Services.AddScoped<PrestamoServices>();
+        builder.Services.AddBlazorBootstrap();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-            var ConStr = builder.Configuration.GetConnectionString("ConStr");
-            builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseStaticFiles();
-            app.UseAntiforgery();
-
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            app.Run();
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
     }
 }
