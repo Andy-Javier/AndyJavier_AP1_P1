86caa43d5626de824483dcb1d00afeef4d2777d3
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-09-30 20:29:29 -0400

    Cree el PrestamoServices

---
+++ Services/PrestamoServices.cs
@@ -0,0 +1,63 @@
+﻿using AndyJavier_AP1_P1.DAL;
+using AndyJavier_AP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+public class PrestamoServices
+{
+    private readonly Contexto _contexto;
+    public PrestamoServices(Contexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    public async Task<bool> Existe(int prestamoId)
+    {
+        return await _contexto.Prestamos.AnyAsync(p => p.PrestamoId == prestamoId);
+    }
+
+    private async Task<bool> Insertar(Prestamo prestamo)
+    {
+        _contexto.Prestamos.Add(prestamo);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Prestamo prestamo)
+    {
+        _contexto.Prestamos.Update(prestamo);
+        var modificado = await _contexto.SaveChangesAsync() > 0;
+        _contexto.Entry(prestamo).State = EntityState.Detached;
+        return modificado;
+    }
+
+    public async Task<bool> Guardar(Prestamo prestamo)
+    {
+        if (!await Existe(prestamo.PrestamoId))
+            return await Insertar(prestamo);
+        else
+            return await Modificar(prestamo);
+    }
+
+    public async Task<bool> Eliminar(int prestamoId)
+    {
+        var eliminarRegistro = await _contexto.Prestamos
+            .Where(p => p.PrestamoId == prestamoId)
+            .ExecuteDeleteAsync();
+        return eliminarRegistro > 0;
+    }
+
+    public async Task<Prestamo?> Buscar(int prestamoId)
+    {
+        return await _contexto.Prestamos
+            .AsNoTracking()
+            .FirstOrDefaultAsync(p => p.PrestamoId == prestamoId);
+    }
+
+    public async Task<List<Prestamo>> Listar(Expression<Func<Prestamo, bool>> criterio)
+    {
+        return await _contexto.Prestamos
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
