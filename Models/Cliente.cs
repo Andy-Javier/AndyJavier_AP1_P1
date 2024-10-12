b70f007aa434b9d83c3554a24092ec3d6ccb9518
Andy Javier <andy_javier@ucne.edu.do>
2024-10-12 00:06:41 -0400

    Cree el Cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AndyJavier_AP1_P1.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string? Nombres { get; set; }
+        public string? Rnc { get; set; }
+        public string? Direccion { get; set; }
+        public decimal LimiteCredito { get; set; }
+    }
+
+}
