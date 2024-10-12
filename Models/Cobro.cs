0d979cf90f1a4b61b527c4cc77c43f2cde274598
Andy Javier <andy_javier@ucne.edu.do>
2024-10-12 00:08:01 -0400

    Cree el Modelo Cobro

---
+++ Models/Cobro.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AndyJavier_AP1_P1.Models
+{
+    public class Cobro
+    {
+        [Key]
+        public int CobroId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int DeudorId { get; set; }
+        public Deudor? Deudor { get; set; }
+        public decimal Monto { get; set; }
+    }
+
+}
