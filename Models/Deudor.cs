d30fad37e2b5c1009f86a2284241b9e0f4e51db5
Andy Javier <andy_javier@ucne.edu.do>
2024-10-12 00:09:06 -0400

    Cree el Modelo Deudor

---
+++ Models/Deudor.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AndyJavier_AP1_P1.Models;
+
+public class Deudor
+{
+    [Key]
+    public int DeudorId { get; set; }
+
+    [Required]
+    public string? Nombres { get; set; }
+}
