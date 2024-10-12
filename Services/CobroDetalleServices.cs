2827799c54626abbaf49e5db5ace6ccc6f363786
Andy Javier <andy_javier@ucne.edu.do>
2024-10-12 00:08:43 -0400

    Cree el CobroDetalleServices

---
+++ Services/CobroDetalleServices.cs
@@ -0,0 +1,44 @@
+﻿using AndyJavier_AP1_P1.DAL;
+using AndyJavier_AP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class CobroDetalleServices
+{
+    private readonly Contexto _contexto;
+
+    public CobroDetalleServices(Contexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    public async Task<List<CobroDetalle>> Listar()
+    {
+        return await _contexto.CobroDetalles.ToListAsync();
+    }
+
+    public async Task<CobroDetalle?> Buscar(int detalleId)
+    {
+        return await _contexto.CobroDetalles.FindAsync(detalleId);
+    }
+
+    public async Task<bool> Insertar(CobroDetalle detalle)
+    {
+        _contexto.CobroDetalles.Add(detalle);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Modificar(CobroDetalle detalle)
+    {
+        _contexto.CobroDetalles.Update(detalle);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Eliminar(int detalleId)
+    {
+        var detalle = await Buscar(detalleId);
+        if (detalle == null) return false;
+
+        _contexto.CobroDetalles.Remove(detalle);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+}
