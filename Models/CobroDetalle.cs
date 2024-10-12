4763bb06113a47b58dd9aadbaa38febb76aa4530
Andy Javier <andy_javier@ucne.edu.do>
2024-10-12 00:08:30 -0400

    Cree el Modelo CobroDetalle

---
+++ Models/CobroDetalle.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AndyJavier_AP1_P1.Models
+{
+    public class CobroDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public int CobroId { get; set; }
+        public Cobro? Cobro { get; set; }
+        public int PrestamoId { get; set; }
+        public Prestamo? Prestamo { get; set; }
+        public decimal ValorCobrado { get; set; }
+    }
+
+}
