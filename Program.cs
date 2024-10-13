e64c9bf621f90672436e028e88704d59a42a5299
Andy Javier <andy_javier@ucne.edu.do>
2024-10-12 23:51:11 -0400

    modifique el program con CobroDetalle y Deudor

--- Program.cs
+++ Program.cs
@@ -1,40 +1,62 @@
 using AndyJavier_AP1_P1.Components;
 using AndyJavier_AP1_P1.DAL;
-using AndyJavier_AP1_P1.Services;
-using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using AndyJavier_AP1_P1.Models;
+using AndyJavier_AP1_P1.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace AndyJavier_AP1_P1;
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-// Obtenemos el ConStr
-var conStr = builder.Configuration.GetConnectionString("ConStr");
-
-// Agregamos el contexto al builder con el ConStr
-builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(conStr));
-
-// Registra los servicios
-builder.Services.AddScoped<PrestamoServices>();
-builder.Services.AddScoped<CobroServices>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        // Obtenemos el ConStr para usarlo en el contexto
+        var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+        // Agregamos el contexto al builder con el ConStr
+        builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
+
+        // Registro de servicios
+        builder.Services.AddScoped<PrestamoServices>();
+        builder.Services.AddScoped<DeudorServices>();
+        builder.Services.AddScoped<CobroServices>();
+        builder.Services.AddScoped<CobroDetalleServices>();
+
+        builder.Services.AddBlazorBootstrap();
+
+        // Habilitar errores detallados para el circuito
+        builder.Services.AddServerSideBlazor()
+            .AddCircuitOptions(options => {
+                options.DetailedErrors = true; // Habilita los errores detallados
+            });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage(); // Esto mostrará errores detallados en desarrollo
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
