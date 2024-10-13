c4ec89e385ebaa8a9c9b1ef7859ccfcae40e69a0
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-10-12 22:43:52 -0400

    cree cobro

---
+++ Models/Cobro.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AndyJavier_AP1_P1.Models;
+
+public class Cobro
+{
+    [Key]
+    public int CobroId { get; set; }
+
+    [Required(ErrorMessage = "Por favor, proporcione una fecha válida.")]
+    public DateTime? Fecha { get; set; }
+
+    public int DeudorId { get; set; }
+
+    [ForeignKey("DeudorId")]
+    public Deudor? Deudor { get; set; }
+
+    [Required(ErrorMessage = "Es necesario especificar el deudor.")]
+    public decimal Monto { get; set; }
+
+    public ICollection<CobroDetalle> CobroDetalles { get; set; } = new List<CobroDetalle>();
+}
