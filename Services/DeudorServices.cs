a871407afebb1692054e6a4ec297efe5e4dd39ea
Andy Javier <andy_javier@ucne.edu.do>
2024-10-12 23:50:33 -0400

    nuevo Deudor services

---
+++ Services/DeudorServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AndyJavier_AP1_P1.DAL;
+using AndyJavier_AP1_P1.Models;
+using System.Linq.Expressions;
+
+namespace AndyJavier_AP1_P1.Services
+{
+    public class DeudorServices
+    {
+        private readonly Contexto _contexto;
+
+        public DeudorServices(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<List<Deudor>> Listar()
+        {
+            return await _contexto.Deudores.AsNoTracking().ToListAsync();
+        }
+    }
+}
