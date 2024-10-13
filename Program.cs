189cb7c337bb6d811f2d3c09b745c78c1d489c8b
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-10-12 22:47:47 -0400

    modifique el program

--- Program.cs
+++ Program.cs
@@ -1,42 +1,40 @@
 using AndyJavier_AP1_P1.Components;
 using AndyJavier_AP1_P1.DAL;
+using AndyJavier_AP1_P1.Services;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
-namespace AndyJavier_AP1_P1;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Obtenemos el ConStr
+var conStr = builder.Configuration.GetConnectionString("ConStr");
+
+// Agregamos el contexto al builder con el ConStr
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(conStr));
+
+// Registra los servicios
+builder.Services.AddScoped<PrestamoServices>();
+builder.Services.AddScoped<CobroServices>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddRazorComponents()
-            .AddInteractiveServerComponents();
-
-        var conStr = builder.Configuration.GetConnectionString("ConStr");
-        builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(conStr));
-
-        // Registrar PrestamoServices
-        builder.Services.AddScoped<PrestamoServices>();
-        builder.Services.AddBlazorBootstrap();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseAntiforgery();
-
-        app.MapRazorComponents<App>()
-            .AddInteractiveServerRenderMode();
-
-        app.Run();
-    }
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
