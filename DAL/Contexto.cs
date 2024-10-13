40234f953cfed2d45159516b3b06c0e8d2b99a29
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-10-12 22:45:34 -0400

    contexto modificado

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,14 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
-using AndyJavier_AP1_P1.Models;
-using Microsoft.Win32;
+﻿using AndyJavier_AP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
-namespace AndyJavier_AP1_P1.DAL;
-
-public class Contexto : DbContext
+namespace AndyJavier_AP1_P1.DAL
 {
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options) { }
 
-    public Contexto(DbContextOptions<Contexto> options)
-        : base(options) { }
-    public DbSet<Prestamo> Prestamos { get; set; }
+        public DbSet<Prestamo> Prestamos { get; set; }
+        public DbSet<Deudor> Deudores { get; set; }
+        public DbSet<Cobro> Cobros { get; set; }
+        public DbSet<CobroDetalle> CobroDetalles { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Deudor>().HasData(new List<Deudor>()
+            {
+                new Deudor() { DeudorId = 1, Nombres = "Samil" },
+                new Deudor() { DeudorId = 2, Nombres = "Maria" },
+                new Deudor() { DeudorId = 3, Nombres = "Juan" }
+            });
+        }
+    }
 }
