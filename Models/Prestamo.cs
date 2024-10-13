64879e59a4b338931f6e97b0ace0916eed7a9c6c
Andy Javier <andy_javier@ucne.edu.do>
2024-10-12 23:54:51 -0400

    añadí mensajes de errores a Prestamo

--- Models/Prestamo.cs
+++ Models/Prestamo.cs
@@ -1,23 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace AndyJavier_AP1_P1.Models
+namespace AndyJavier_AP1_P1.Models;
+
+public class Prestamo
 {
-    public class Prestamo
-    {
-        [Key]
-        public int PrestamoId { get; set; }
+    [Key]
+    public int PrestamoId { get; set; }
 
-        [Required]
-        public int DeudorId { get; set; } 
+    [Required(ErrorMessage = "El campo 'Concepto' es obligatorio.")]
+    public string? Concepto { get; set; }
 
-        public Deudor? Deudor { get; set; } 
+    [Required(ErrorMessage = "El campo 'Monto' es obligatorio.")]
+    public decimal Monto { get; set; }
 
-        [Required]
-        public string? Concepto { get; set; }
+    [Required(ErrorMessage = "El campo 'Balance' es obligatorio.")]
+    public decimal Balance { get; set; }
 
-        [Required]
-        public int Monto { get; set; }
+    [Required(ErrorMessage = "El campo 'DeudorId' es obligatorio.")]
+    public int DeudorId { get; set; }
 
-        public int Balance { get; set; }
-    }
+    [ForeignKey("DeudorId")]
+    public Deudor? Deudor { get; set; }
+
+    public List<CobroDetalle> CobroDetalles { get; set; } = new List<CobroDetalle>();
 }
