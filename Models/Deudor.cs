9af79838ffd9f5c142c2d418134f9c249970edef
Andy Vladimir Javier Familia <andy_javier@ucne.edu.do>
2024-10-12 22:44:21 -0400

    cree deudor

---
+++ Models/Deudor.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AndyJavier_AP1_P1.Models;
+
+public class Deudor
+{
+    [Key]
+    public int DeudorId { get; set; }
+
+    [Required(ErrorMessage = "Por favor, ingrese el nombre del deudor.")]
+    public string? Nombres { get; set; }
+}
