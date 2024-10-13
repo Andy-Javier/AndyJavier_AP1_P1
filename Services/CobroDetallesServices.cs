f66f566abcb32ac5790ce5fdfd2bb3e0b1bcbc2a
Andy Javier <andy_javier@ucne.edu.do>
2024-10-12 23:50:02 -0400

    cree cobroDetallesServices

---
+++ Services/CobroDetallesServices.cs
@@ -0,0 +1,84 @@
+﻿using AndyJavier_AP1_P1.DAL;
+using AndyJavier_AP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AndyJavier_AP1_P1.Services
+{
+    public class CobroDetalleServices
+    {
+        private readonly Contexto _contexto;
+
+        public CobroDetalleServices(Contexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<bool> Existe(int cobroDetalleId)
+        {
+            return await _contexto.CobroDetalles.AnyAsync(cd => cd.DetalleId == cobroDetalleId);
+        }
+
+        private async Task<bool> Insertar(CobroDetalle cobroDetalle)
+        {
+            _contexto.CobroDetalles.Add(cobroDetalle);
+            return await _contexto.SaveChangesAsync() > 0;
+        }
+
+        private async Task<bool> Modificar(CobroDetalle cobroDetalle)
+        {
+            _contexto.CobroDetalles.Update(cobroDetalle);
+            var modificado = await _contexto.SaveChangesAsync() > 0;
+
+            _contexto.Entry(cobroDetalle).State = EntityState.Detached;
+
+            return modificado;
+        }
+
+        public async Task<bool> Guardar(CobroDetalle cobroDetalle)
+        {
+            if (!await Existe(cobroDetalle.DetalleId))
+                return await Insertar(cobroDetalle);
+            else
+                return await Modificar(cobroDetalle);
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            var eliminarCobroDetalle = await _contexto.CobroDetalles
+                .Where(cd => cd.DetalleId == id)
+                .ExecuteDeleteAsync();
+
+            return eliminarCobroDetalle > 0;
+        }
+
+        public async Task<CobroDetalle?> Buscar(int id)
+        {
+            return await _contexto.CobroDetalles
+                .AsNoTracking()
+                .Include(cd => cd.Cobro)
+                .Include(cd => cd.Prestamo)
+                .FirstOrDefaultAsync(cd => cd.DetalleId == id);
+        }
+
+        public async Task<List<CobroDetalle>> Listar(Expression<Func<CobroDetalle, bool>> criterio)
+        {
+            return await _contexto.CobroDetalles
+                .AsNoTracking()
+                .Include(cd => cd.Cobro)
+                .Include(cd => cd.Prestamo)
+                .Where(criterio)
+                .ToListAsync();
+        }
+
+        public async Task<List<Deudor>> ObtenerDeudores()
+        {
+            return await _contexto.Deudores.ToListAsync();
+        }
+
+        public async Task<List<Prestamo>> ObtenerPrestamos()
+        {
+            return await _contexto.Prestamos.ToListAsync();
+        }
+    }
+}
